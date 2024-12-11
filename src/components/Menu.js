98145a90da5ecccd46ba96c25ba684a0d7ce63cb
gmartinez7435 <gmartinez7435@gmail.com>
2024-12-10 20:26:54 -0700

    completed the specials for the main screen

---
+++ src/components/Menu.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import recipes from '../recipes'
+import Swal from 'sweetalert2';
+
+const Menu = () => {
+
+    const handleOrder = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "Add this to your cart?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, order it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Order confirmed!",
+                text: "Your order has been confirmed.",
+                icon: "success"
+              });
+            }
+          });
+    }
+
+  return (
+    <div className='menu-container'>
+        <div className='menu-header'>
+            <h2>This Weeks Specials!</h2>
+            <button>Order Now</button>
+        </div>
+        <div className='cards'>
+            {
+                recipes.map(recipe => <div key={recipe.id} className='menu-items'>
+                    <img src={recipe.image} alt="special items"/>
+                    <div className='menu-content'>
+                        <div className='heading'>
+                            <h5>{recipe.title}</h5>
+                            <p>{recipe.price}</p>
+                        </div>
+                        <p>{recipe.description}</p>
+                        <button className='orderBtn' onClick={() => handleOrder(recipe.id)}>Order Now</button>
+                    </div>
+                </div>)
+            }
+
+        </div>
+    </div>
+  )
+}
+
+export default Menu
