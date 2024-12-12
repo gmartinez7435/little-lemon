d38e2b65d4a2c2bc1dd788fab01fdadeb9bf82c0
gmartinez7435 <gmartinez7435@gmail.com>
2024-12-12 00:57:01 -0700

    fixed footer spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ import small_logo from '../assets/small_logo.png'
 const Footer = () => {
   return (
     <footer>
-        <section>
+        <section className='foot-links'>
             <div className='company-info'>
                 <img src={small_logo} alt="logo" />
             </div>
-            <div>
+            <div className='foot-section'>
               <h3>Important Links</h3>
               <ul>
                 <li><a href='/'>Home</a></li>
@@ -16,18 +16,18 @@ const Footer = () => {
                 <li><a href='/menu'>Menu</a></li>
                 <li><a href='/booking'>Reservations</a></li>
                 <li><a href='/order-online'>Order Online</a></li>
-                <li><a href='/login'>Login</a></li>
+                <li><a href='/signup'>Login</a></li>
               </ul>
             </div>
 
-            <div>
+            <div className='foot-section'>
               <h3>Contact Us</h3>
               <p>123 Main St, Chicago, Illinois 12345</p>
               <p>Phone: 123-456-7890</p>
               <p>Email: littlelemon@example.com</p>
             </div>
 
-            <div>
+            <div className='foot-section'>
               <h3>Social Media Links</h3>
               <ul>
                 <li><a href='facebook.com'>Facebook</a></li>
