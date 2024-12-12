541d19c756af7108dd02c87134790e1abd042d73
gmartinez7435 <gmartinez7435@gmail.com>
2024-12-12 01:51:07 -0700

    made the menu page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import MenuItems from '../menuitems';
+
 
 const Menu = () => {
-  return (
-    <div>Menu</div>
-  )
-}
 
-export default Menu
+    return (
+      <div className='menu-container'>
+        <div className='menu-header'>
+            <h2>This Months Menu!</h2>
+        </div>
+        <div className='cards'>
+          {
+            MenuItems.map(item => (
+              <div key={item.id} className='menu-items'>
+                <img src={item.image} alt={item.title} />
+                <div className='menu-content'>
+                  <div className='heading'>
+                    <h5>{item.title}</h5>
+                    <p>${item.price.toFixed(2)}</p>
+                  </div>
+                  <p>{item.description}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    );
+};
+
+export default Menu;
---
+++ src/menuitems.js
@@ -0,0 +1,93 @@
+const MenuItems = [
+  {
+    id: 1,
+    title: "Greek Salad",
+    price: 12.99,
+    image: "https://i.ibb.co/68c9bLv/greek-salad.jpg",
+    description:
+      "Crispy lettuce, peppers, olives, and Chicago-style feta cheese garnished with garlic and rosemary croutons.",
+  },
+  {
+    id: 2,
+    title: "Bruschetta",
+    price: 5.99,
+    image: "https://i.ibb.co/317GqZv/Bruchetta.png",
+    description:
+      "Grilled bread smeared with garlic and seasoned with salt and olive oil.",
+  },
+  {
+    id: 3,
+    title: "Lemon Dessert",
+    price: 4.99,
+    image: "https://i.ibb.co/sm6MfcH/desert.jpg",
+    description:
+      "Straight from grandma's recipe book, every ingredient is sourced and authentic.",
+  },
+  {
+    id: 4,
+    title: "Falafel",
+    price: 6.99,
+    image: "https://media.istockphoto.com/id/590061862/photo/falafel-deep-fried-balls-of-ground-chickpeas.jpg?s=612x612&w=0&k=20&c=HIvyGqSZ0Tc5HjeZ96xHRFEUIohGrWhxxQSPsIPUsf4=",
+    description: "Crispy chickpea fritters served with tahini sauce.",
+  },
+  {
+    id: 5,
+    title: "Shish Kebab",
+    price: 14.99,
+    image: "https://images.pexels.com/photos/6675489/pexels-photo-6675489.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description: "Grilled skewers of marinated meat or vegetables.",
+  },
+  {
+    id: 6,
+    title: "Baklava",
+    price: 5.49,
+    image: "https://images.pexels.com/photos/7393898/pexels-photo-7393898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description:
+      "Layers of phyllo dough filled with nuts and sweetened with honey syrup.",
+  },
+  {
+    id: 7,
+    title: "Hummus",
+    price: 4.49,
+    image: "https://media.istockphoto.com/id/955998652/photo/homemade-chickpea-hummus-bowl-with-pita-chips-and-paprika.jpg?s=612x612&w=0&k=20&c=H3m78rhd7113mc06CqBKLH4PVn7e8AUmTes2yCQEzlY=",
+    description: "Creamy chickpea dip served with warm pita bread.",
+  },
+  {
+    id: 8,
+    title: "Tzatziki",
+    price: 3.99,
+    image: "https://media.istockphoto.com/id/1272446274/photo/traditional-greek-yogurt-tzatziki-sauce-with-cucumbers-and-herbs-on-wooden-background.jpg?s=612x612&w=0&k=20&c=1pcm2kfrD4ARFfA3PJSilxbiexU9x-Trhx5lhidIV3Q=",
+    description: "Yogurt dip with cucumber, garlic, and dill.",
+  },
+  {
+    id: 9,
+    title: "Dolmas",
+    price: 5.99,
+    image: "https://media.istockphoto.com/id/1263363580/photo/traditional-dish-dolma.jpg?s=612x612&w=0&k=20&c=sO0uGNLMb7V1VngP_arrPET6nfwo5pEnPJAFmGupy90=",
+    description: "Grape leaves stuffed with rice and herbs.",
+  },
+  {
+    id: 10,
+    title: "Spanakopita",
+    price: 6.49,
+    image: "https://media.istockphoto.com/id/1300215085/photo/spanakopita-with-tzatziki-sauce.jpg?s=612x612&w=0&k=20&c=O3HDK4Qaq6x5oARlSqZ3RCRS0AgZ9JIYcqzXuPpLQko=",
+    description: "Spinach and feta cheese wrapped in phyllo pastry.",
+  },
+  {
+    id: 11,
+    title: "Moussaka",
+    price: 13.99,
+    image: "https://media.gettyimages.com/id/1136417746/photo/moussaka-with-minced-meat.jpg?s=612x612&w=0&k=20&c=CrIAcqtJxbjzhcFnge57BANL1VkD1qex9tuPGO7Gdug=",
+    description:
+      "Layered eggplant, minced meat, and béchamel sauce baked to perfection.",
+  },
+  {
+    id: 12,
+    title: "Seafood Paella",
+    price: 18.99,
+    image: "https://media.istockphoto.com/id/607991782/photo/traditional-spanish-paella-with-seafood-and-chicken.jpg?s=612x612&w=0&k=20&c=m7pKIt42ihApJd89_F6Nb8w7Eg9ZIn5Mw6YBXW3-Xxc=",
+    description:
+      "Rice dish with a variety of seafood, saffron, and vegetables.",
+  },
+];
+export default MenuItems;
