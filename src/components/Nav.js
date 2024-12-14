cf3ab5aa80c015f91066f70c0e40e87fd582ebee
gmartinez7435 <gmartinez7435@gmail.com>
2024-12-14 01:24:24 -0700

    added basket and fixed the sizes of sub menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,53 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/Logo.svg';
+import Basket from '../assets/Basket.svg';
 
 const Nav = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
-    }
+    };
 
     const closeMenu = () => {
-      setMenuOpen(false);
-    }
+        setMenuOpen(false);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 768) {
+                closeMenu();
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <nav className={`navbar ${menuOpen ? "open" : ""}`}>
             <Link to="/" className='logo'>
                 <img src={Logo} alt="Little Lemon Restaurant" />
             </Link>
-            {/* for Mobile */}
             <div className='menu-icon' onClick={toggleMenu}>
                 <div className='bar'></div>
                 <div className='bar'></div>
                 <div className='bar'></div>
             </div>
-            {/* nav links */}
             <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
                 <li>
-                  <Link to="/" onClick={closeMenu}>Home</Link>
+                    <Link to="/" onClick={closeMenu}>Home</Link>
                 </li>
                 <li>
-                  <Link to="/About" onClick={closeMenu}>About</Link>
+                    <Link to="/About" onClick={closeMenu}>About</Link>
                 </li>
                 <li>
-                  <Link to="/Menu" onClick={closeMenu}>Menu</Link>
+                    <Link to="/Menu" onClick={closeMenu}>Menu</Link>
                 </li>
                 <li>
-                  <Link to="/Booking" onClick={closeMenu}>Reservations</Link>
+                    <Link to="/Booking" onClick={closeMenu}>Reservations</Link>
                 </li>
                 <li>
-                  <Link to="/Order" onClick={closeMenu}>Order Online</Link>
+                    <Link to="/Order" onClick={closeMenu}>Order Online</Link>
                 </li>
                 <li>
-                  <Link to="/Signup" onClick={closeMenu}>Sign-up/Login</Link>
+                    <Link to="/Signup" onClick={closeMenu}>Sign-up/Login</Link>
                 </li>
+                <img style={{ maxWidth: "35px" }} src={Basket} alt='basket icon'></img>
             </ul>
-            {/* for desktop */}
         </nav>
-    )
-}
+    );
+};
 
 export default Nav;
